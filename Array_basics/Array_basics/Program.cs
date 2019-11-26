54e0b9d5e278855836d934ccacdaf4a1a77c7d87
Krunoslav Stefanek <prof.stefanek@gmail.com>
2019-11-26 10:17:17 +0100

    Array generator
    In comments added a generator of strings of type A000

--- Array_basics/Array_basics/Program.cs
+++ Array_basics/Array_basics/Program.cs
@@ -25,6 +25,31 @@ namespace Array_basics
 
             // or
             // string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+
+            //or 
+            /*
+            The following code creates five random OrderIDs
+            to test the fraud detection process.  OrderIDs 
+            consist of a letter from A to E, and a three
+               digit number. Ex. A123.
+
+            Random random = new Random();
+            string[] orderIDs = new string[5];
+
+            for (int i = 0; i < orderIDs.Length; i++)
+            {
+                int prefixValue = random.Next(65, 70);
+                string prefix = Convert.ToChar(prefixValue).ToString();
+                string suffix = random.Next(1, 1000).ToString("000");
+
+                orderIDs[i] = prefix + suffix;
+            }
+            */
+            foreach (var orderID in orderIDs)
+            {
+                Console.WriteLine(orderID);
+            }
             for (int i = 0; i < fraudulentOrderIDs.Length; i++)
             {
                 Console.WriteLine(fraudulentOrderIDs[i]);
