1a26e2dcdaebd59efaac40e2a2b3fb6a066841b5
Krunoslav Stefanek <prof.stefanek@gmail.com>
2019-12-20 10:44:23 +0100

    Conditional operators inline
    Flip coin situation with random generator of result and inline conditional operator

---
+++ Conditional_operators_inline/Conditional_operators_inline/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Conditional_operators_inline
+{
+    class Program
+    {
+    
+            static void Main(string[] args)
+            {
+                //How to implement conditional operator in program
+                Random coin = new Random();
+                int flip = coin.Next(0, 2);
+                Console.WriteLine($"You flipped coin and got :{(flip < 1 ? "head" : "tails")}");
+            }
+        
+    }
+
+}
