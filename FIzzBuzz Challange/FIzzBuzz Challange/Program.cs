d79ea33ecbadeaf26f3d322f457ed9d65844bc60
Krunoslav Stefanek <prof.stefanek@gmail.com>
2020-02-03 18:17:24 +0100

    FizzBuzz Challange
    Actual FizzBuzz Challenge

---
+++ FIzzBuzz Challange/FIzzBuzz Challange/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*FizzBuzz RULES: FizzBuzz rules:
+
+            Output values from 1 to 100, one number per line.
+            When the current value is divisible by 3, print the term Fizz next to the number.
+            When the current value is divisible by 5, print the term Buzz next to the number.
+            When the current value is divisible by both 3 and 5, print the term FizzBuzz next to the number.*/
+
+            Console.WriteLine("Hello World!");
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0) Console.Write($"{i} Fizz\n ");
+                else if (i % 5 == 0 && i % 3 != 0) Console.Write($"{i} Buzz\n ");
+                else if (i % 3 == 0 && i % 5 == 0) Console.Write($"{i} FizzBuzz\n ");
+                else Console.WriteLine(i);
+
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
